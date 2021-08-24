3534a3c5419232214bb080e6dc5ab2a50dea8ba8
Himanshu <himanshugohil234@gmail.com>
2021-08-24 11:59:42 +0530

    added build folder

---
+++ client/build/assets/js/main.js
@@ -0,0 +1,41 @@
+(function ($) {
+  $(document).ready(function () {
+    var a = 0;
+    var f = $("#figures");
+    if (f.length) {
+      $(window).scroll(function () {
+        var oTop = $("#figures").offset().top - window.innerHeight;
+        if (a === 0 && $(window).scrollTop() > oTop) {
+          $(".figure-amt-inner").each(function () {
+            var $this = $(this),
+              countTo = $this.attr("data-counter");
+            $({
+              countNum: $this.text(),
+            }).animate(
+              {
+                countNum: countTo,
+              },
+
+              {
+                duration: 2000,
+                easing: "swing",
+                step: function () {
+                  $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                  $this.text(this.countNum);
+                  //alert('finished');
+                },
+              }
+            );
+          });
+          a = 1;
+        }
+      });
+    }
+
+    $(".sidbar-toggle").on("click", function (e) {
+      $("#sidebar").slideToggle();
+    });
+  });
+})(jQuery);
